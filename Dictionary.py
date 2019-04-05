b6bdcb46f4d91d6c87458d674b0472470c533b37
archirick <48961843+archirick@users.noreply.github.com>
2019-04-05 18:24:42 +0530

    Add files via upload

---
+++ Dictionary.py
@@ -0,0 +1,13 @@
+##Dictionary
+dict={1:"google",2:"intel",3:"microsoft",4:"amd"}
+print(dict.keys())
+print(dict.values())
+
+##Inplementation of values
+
+for i in dict:
+    print(dict[i])
+
+##adding more elements
+dict[5]='sony'
+print(dict)
